c1b207825076dfb2174aad2a2f3bdbb9dada2d70
Jesus <jesusadadfilho@gmail.com>
2018-05-15 02:11:07 -0300

    Tela de login, main e cadastro incompleta

--- app/src/main/java/br/com/m2j/proteseanalytics/app/LoginActivity.java
+++ app/src/main/java/br/com/m2j/proteseanalytics/app/LoginActivity.java
@@ -2,6 +2,7 @@ package br.com.m2j.proteseanalytics.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import br.com.m2j.proteseanalytics.R;
 
@@ -11,5 +12,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void logar(View view) {
+        //Todo::logar
     }
 }
---
+++ app/src/main/java/br/com/m2j/proteseanalytics/app/MainActivity.java
@@ -0,0 +1,34 @@
+package br.com.m2j.proteseanalytics.app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import br.com.m2j.proteseanalytics.R;
+import br.com.m2j.proteseanalytics.app.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void realizarLogin(View view) {
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void realizarCadastro(View view) {
+        //ToDO::cadastro
+    }
+
+    public void autentificarComFacebook(View view) {
+        //ToDO::loginFacebook
+    }
+
+    public void autentificarComGoogle(View view) {
+        //ToDO::loginFacebook
+    }
+}
