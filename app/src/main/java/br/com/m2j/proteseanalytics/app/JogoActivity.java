d03b605e59b3f66474fccc9741f07fb26a4e0176
Jesus <jesusadadfilho@gmail.com>
2018-05-21 20:28:29 -0300

    tela do jogo

---
+++ app/src/main/java/br/com/m2j/proteseanalytics/app/JogoActivity.java
@@ -0,0 +1,13 @@
+package br.com.m2j.proteseanalytics.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogo);
+    }
+}
--- app/src/test/java/br/com/m2j/proteseanalytics/model/Dados.java
+++
@@ -1,8 +0,0 @@
-package br.com.m2j.proteseanalytics.model;
-
-/**
- * Created by Marcelo on 14/05/2018.
- */
-
-public class Dados {
-}
