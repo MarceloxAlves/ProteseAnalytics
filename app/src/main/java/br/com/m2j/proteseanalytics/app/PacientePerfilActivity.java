72ade9dc5a0a20042378ad29195d8f03b496b789
Jesus <jesusadadfilho@gmail.com>
2018-05-21 00:57:33 -0300

    tela de perfil

---
+++ app/src/main/java/br/com/m2j/proteseanalytics/app/PacientePerfilActivity.java
@@ -0,0 +1,15 @@
+package br.com.m2j.proteseanalytics.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.m2j.proteseanalytics.R;
+
+public class PacientePerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paciente_perfil);
+    }
+}
